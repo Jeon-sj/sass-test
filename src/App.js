3d974950ba33056c00876b86a17de4888c99fb59
demonj <demonj73@gmail.com>
2021-04-30 14:22:06 +0900

    second commit

--- src/App.js
+++ src/App.js
@@ -6,7 +6,30 @@ function App() {
   return (
     <div className="App">
       <div className="buttons">
+        <Button size="large">BUTTON</Button>
         <Button>BUTTON</Button>
+        <Button size="small">BUTTON</Button>
+      </div>
+      <div className="buttons">
+        <Button size="large" color="gray">BUTTON</Button>
+        <Button color = "gray">BUTTON</Button>
+        <Button size = "small" color="gray">BUTTON</Button>
+      </div>
+      <div className="buttons">
+        <Button size="large" color="pink">BUTTON</Button>
+        <Button color = "pink">BUTTON</Button>
+        <Button size = "small" color="pink">BUTTON</Button>
+      </div>
+      <div className="buttons">
+        <Button size="large" color="blue" outline>BUTTON</Button>
+        <Button color = "gray" outline>BUTTON</Button>
+        <Button size = "small" color="pink" outline>BUTTON</Button>
+      </div>
+      <div className="buttons">
+        <Button fullWidth>BUTTON</Button>
+        <Button fullWidth color = "gray">BUTTON</Button>
+        <Button fullWidth color = "pink">BUTTON</Button>
+        
       </div>
     </div>
   );
